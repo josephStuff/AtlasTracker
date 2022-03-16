198da00dedc0e9524d340c63063cec1cc561513f
josephStuff <mrwatsonjoe28@yahoo.com>
2022-03-16 16:26:43 -0400

    updated NuGetPackage and more /template/ src= things

--- AtlasTracker/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AtlasTracker/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -104,7 +104,7 @@ namespace AtlasTracker.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/Home/Dashboard");
+            returnUrl ??= Url.Content("~/Home/Default");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
