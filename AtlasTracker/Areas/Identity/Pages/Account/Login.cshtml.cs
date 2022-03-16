a050bf1decb1dcd3b09038df29d6efbf4fa2c1ef
josephStuff <mrwatsonjoe28@yahoo.com>
2022-03-16 12:20:04 -0400

    added Interface for notifications and changed the default to dashboard

--- AtlasTracker/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AtlasTracker/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -104,7 +104,7 @@ namespace AtlasTracker.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Home/Default");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
