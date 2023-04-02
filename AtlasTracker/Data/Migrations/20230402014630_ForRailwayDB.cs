7aa4b186463a72a6c0bdbf8942a5e3c1047aa20d
josephStuff <mrwatsonjoe28@yahoo.com>
2023-04-01 22:16:59 -0400

    added a new DB to see if it will upload into Railway

---
+++ AtlasTracker/Data/Migrations/20230402014630_ForRailwayDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtlasTracker.Data.Migrations
+{
+    public partial class ForRailwayDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
